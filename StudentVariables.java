cc897332b218ad724f682d918f6340be46117b54
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-02-08 21:04:08 +0530

    Primitive Data Types

---
+++ StudentVariables.java
@@ -0,0 +1,14 @@
+public class StudentVariables {
+
+    public static void main(String[] args){
+        int studentAge = 15; // Integer
+        double studentGpa = 4.5; // Float
+        char studentFirstInitial = 'V'; // Character
+        boolean hasPerfectAttendance = true; // Boolean
+
+        System.out.println(studentAge);
+        System.out.println(studentGpa);
+        System.out.println(studentFirstInitial);
+        System.out.println(hasPerfectAttendance);
+    }
+}
