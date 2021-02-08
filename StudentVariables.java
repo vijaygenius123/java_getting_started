2217a3f87d2755f6d43467752dead01445296075
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-02-08 21:09:59 +0530

    Strings

--- StudentVariables.java
+++ StudentVariables.java
@@ -1,14 +1,20 @@
 public class StudentVariables {
 
     public static void main(String[] args){
-        int studentAge = 15; // Integer
-        double studentGpa = 4.5; // Float
-        char studentFirstInitial = 'V'; // Character
-        boolean hasPerfectAttendance = true; // Boolean
+        // int studentAge = 15; // Integer
+        // double studentGpa = 4.5; // Float
+        // char studentFirstInitial = 'V'; // Character
+        // boolean hasPerfectAttendance = true; // Boolean
+        String studentFirstname = "Vijayaraghavan";
+        String studentLastname = "Sundararaman";
 
-        System.out.println(studentAge);
-        System.out.println(studentGpa);
-        System.out.println(studentFirstInitial);
-        System.out.println(hasPerfectAttendance);
+        // System.out.println(studentAge);
+        // System.out.println(studentGpa);
+        // System.out.println(studentFirstInitial);
+        // System.out.println(hasPerfectAttendance);
+        System.out.println(studentFirstname);
+        System.out.println(studentLastname);
+        System.out.println(studentFirstname.charAt(0));
+        System.out.println(studentFirstname.charAt(studentFirstname.length() - 1));
     }
 }
