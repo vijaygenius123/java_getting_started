8a30c13386187589ca25ad9488a240c0e9cef178
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-02-09 23:39:05 +0530

    For Loops

---
+++ ForLoops.java
@@ -0,0 +1,13 @@
+public class ForLoops {
+
+  public static void main(String[] args){
+
+    int count = 5;
+
+    for(int i=0;i<count;i++){
+      System.out.println("Count: " + i);
+    }
+
+  }
+
+}
